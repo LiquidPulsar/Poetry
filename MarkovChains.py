119cc26f56f914dcc9b147c73fcfa1ff33672881
Alex Noel <66466249+LiquidPulsar@users.noreply.github.com>
2020-10-19 16:02:42 +0100

    Update MarkovChains.py

--- MarkovChains.py
+++ MarkovChains.py
@@ -1,1 +1,10 @@
+from gutenberg.acquire import load_etext
+from gutenberg.cleanup import strip_headers
+from gutenberg.query import get_etexts
 
+def GetTexts(method = "Title",_key):
+  if method == "Title":
+    Stuff = get_etexts('title', _key))
+    text = set(strip_headers(load_etext(index)).strip() for index in Stuff)
+    return text
+    
